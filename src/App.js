979927fc3c2b98926924ae23f373c72cdee1e54b
yi-seul <rim4291@naver.com>
2022-12-27 16:49:16 +0900

    init2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import LayoutHeader from './components/Layout/header/Header';
+import HomePage from './pages/Home';
+import GlobalStyles from './styles/global';
+import { theme } from './styles/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /*     Axios.get('/movie/popular', {
+        params: {
+            language: 'ko-KO',
+            page: 2,
+        },
+    }).then((res) => {
+        console.log(res);
+    }); */
+    // movie/popular?laungage=ko-KO&page=2
+
+    const qureyClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                staleTime: 1000 * 60 * 60 * 24,
+                cacheTime: 1000 * 60 * 5,
+            },
+        },
+    });
+
+    return (
+        <QueryClientProvider client={qureyClient}>
+            <ReactQueryDevtools initialIsOpen={true} />
+            <ThemeProvider theme={theme}>
+                <GlobalStyles />
+                <LayoutHeader />
+                <HomePage />
+            </ThemeProvider>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
